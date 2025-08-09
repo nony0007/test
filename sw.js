cbcfb2ef91aa0349ff808fa017dfb50b1b2f50ef
nony0007 <90529894+nony0007@users.noreply.github.com>
2025-08-09 18:20:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('fops-v2-1-1').then(c=>c.addAll(['./','./index.html?v=2.1.1','./404.html','./style.css?v=2.1.1','./app.js?v=2.1.1','./data.js','./manifest.json'])));
+  e.waitUntil(caches.open('fops-v2-1-3').then(c=>c.addAll(['./','./index.html?v=2.1.3','./404.html','./style.css?v=2.1.3','./app.js?v=2.1.3','./data.js','./manifest.json?v=2.1.3'])));
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
